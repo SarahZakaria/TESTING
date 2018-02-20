d38974257d73805bec972dbd2b76d13ad27ab0ae
SarahZakaria <36151511+SarahZakaria@users.noreply.github.com>
2018-02-20 16:01:12 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<title>sistem pinjaman buku di perpustakaan</title>
+<html lang="en">
+
+<head>
+         <meta charset="utf-8" />
+         <meta name="description" content="Fishing in Dorset">
+         <meta name="robots" content="noindex">
+      
+		 <!--link-->
+	<link rel="stylesheet" type="text/css" href="1.css">	
+	<link rel="stylesheet" type="text/css" href="2.css">	
+<link rel="shortcut icon" href="image/books.jpg" type="image/books.jpg" /> 
+	<link rel="javascript" type="text/js" href="j2.js">
+</head>
+
+<!--login form-->
+<div class="admin">
+<div class="dropdown" style="float:right;">
+  <button class="dropbtn">LOGIN</button>
+  <div class="dropdown-content">
+	  <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">USER</button>
+	 <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">ADMIN</button>
+ </div>
+</div>
+
+
+  <div id="id01" class="modal">
+  
+  <form class="modal-content animate" action="homepage admin.php">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+      <img src="image/pc1.jpg" alt="pc1" class="pc1">
+    </div>
+
+    <div class="container">
+      <label><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required>
+
+      <label><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+        
+      <button type="submit">Login</button>
+      <label>
+        <input type="checkbox" checked="checked"> Remember me
+      </label>
+    </div>
+
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+      <span class="psw">Forgot <a href="password.php">password?</a></span>
+   </div>
+  </form>
+</div>
+
+
+     </div>
+</div>
+ 
+ 
+<div class="admin">
+
+
+ 
+  <div id="id01" class="modal">
+  
+  <form class="modal-content animate" action="/action_page.php">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+      <img src="image/pc1.jpg" alt="pc1" class="pc1">
+    </div>
+
+    <div class="container">
+      <label><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required>
+
+      <label><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+        
+      <button type="submit">Login</button>
+      <label>
+        <input type="checkbox" checked="checked"> Remember me
+      </label>
+    </div>
+
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+      <span class="psw">Forgot <a href="password.php">password?</a></span>
+  </div>
+  </form>
+</div>
+
+
+     </div>
+</div>
+<center>
+ <div class="row">
+
+<div class="leftcolumn">
+<div class="card">
+     <h1><i>PERPUSTAKAAN PENGKALAN HULU</h1>
+<br>
+<img src="image/2.jpg" height="350" width="750"></th>
+	</center>
+
+ 
+</ul>
+
+
+</style>
+</head>
+
+</body>
+</html>
